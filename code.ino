07b79649415ed427e5a4c7813584adaa42df2353
Kesavan Rangarajan <k7rang2018@gmail.com>
2024-03-24 17:52:25 -0400

    it workies

--- code.ino
+++ code.ino
@@ -1,125 +1,20 @@
-#define IN1_A 5  // Motor A IN1 pin
-#define IN2_A 7  // Motor A IN2 pin
-#define IN1_B 6  // Motor B IN1 pin
-#define IN2_B 8  // Motor B IN2 pin
-#define EN_A  9  // Motor A enable pin
-#define EN_B  10 // Motor B enable pin
+#define motor1pin1 2  // Motor A IN1 pin
+#define motor1pin2 3  // Motor A IN2 pin
+#define motor2pin1 4  // Motor B IN1 pin
+#define motor2pin2 5  // Motor B IN2 pin
+// #define EN_A  9  //  Motor A enable pin
+// #define EN_B  10 // Motor B enable pin
 #include "HUSKYLENS.h"
 #include "SoftwareSerial.h"
+#include <arduino-timer.h>
+// #define huskyLens1 0 // huskyLens pin 1
+// #define huskyLens2 1 // huskyLens pin 2
+auto timer = timer_create_default();
+SoftwareSerial mySerial(10, 11);
 
-char prevChoice = "rock";
-int cC = "";//current choice 1 = rock 2 = paper 3 = scisors
+// char cC = "rock";
+int cC;//current choice rock = 1, scissors = 2, paper = 3
 HUSKYLENS huskylens;
-
-void setup() {
-  pinMode(IN1_A, OUTPUT);
-  pinMode(IN2_A, OUTPUT);
-  pinMode(IN1_B, OUTPUT);
-  pinMode(IN2_B, OUTPUT);
-  pinMode(EN_A, OUTPUT);
-  pinMode(EN_B, OUTPUT);
-
-  rock();
-}
-
-void loop() {
-  if (!huskylens.request()) {
-Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection!"));
-}
-    else if(!huskylens.isLearned()){ 
-Serial.println(F("Nothing learned, press learn button on HUSKYLENS to learn one!"));
-}
-    else if(!huskylens.available()) {
-Serial.println(F("No block or arrow appears on the screen!"));
-}
-    else
-    {
-        Serial.println(F("###########"));
-        while (huskylens.available())
-        {
-            HUSKYLENSResult result = huskylens.read();
-            printResult(result);
-            sense(result);
-        }    
-    }
-  long random = randomChoice();
-  if (random == 1) {
-    rock();
-    cC = 1;
-  }
-  else if (random == 2) {
-    paper();
-    cC = 2;
-  }
-  else {
-    scissors();
-    cC = 3;
-  }
-  
-
-}
-
-void motor1Forward() {
-  digitalWrite(IN1_A, HIGH);
-  digitalWrite(IN2_A, LOW);
-  digitalWrite(EN_A, HIGH);
-}
-
-void motor1Backward() {
-  digitalWrite(IN1_A, LOW);
-  digitalWrite(IN2_A, HIGH);
-  digitalWrite(EN_A, HIGH);
-}
-
-void motor2Forward() {
-  digitalWrite(IN1_B, HIGH);
-  digitalWrite(IN2_B, LOW);
-  digitalWrite(EN_B, HIGH);
-}
-
-void motor2Backward() {
-  digitalWrite(IN1_B, LOW);
-  digitalWrite(IN2_B, HIGH);
-  digitalWrite(EN_B, HIGH);
-}
-
-// Needs hand in Rock position before
-void scissors() {
-  if (prevChoice == "rock") {
-    motor1Forward();
-  }
-  else if (prevChoice == "paper") {
-    motor2Backward();
-  }
-  prevChoice = "scissors";
-}
-// needs hand in paper position before
-void rock() {
-  if (prevChoice == "paper"){
-    motor1Backward();
-    motor2Backward();
-  }
-  else if (prevChoice == "scissors") {
-    motor1Backward();
-  }
-  prevChoice = "rock";
-}
-// needs hand in rock position before
-void paper() {
-  if (prevChoice == "rock"){
-    motor1Forward();
-    motor2Forward();
-  }
-  else if (prevChoice == "scissors") {
-    motor2Forward();
-  }
-  prevChoice = "paper";
-}
-
-long randomChoice() {
-  return random(1, 4) ;
-}
-
 
 void printResult(HUSKYLENSResult result){
     if (result.command == COMMAND_RETURN_BLOCK){
@@ -132,43 +27,208 @@ void printResult(HUSKYLENSResult result){
         Serial.println("Object unknown!");
     }
 }
+int getValue(HUSKYLENSResult result){
+  if(result.ID == 1|| result.ID == 4|| result.ID == 7|| result.ID == 10) {
+    return 1;
+  }
+  else if (result.ID == 2 || result.ID == 5 || result.ID == 8 || result.ID == 11) {
+    return 2;
+  }
+  else if (result.ID == 3|| result.ID == 6|| result.ID == 9|| result.ID == 12) {
+    return 3;
+  }
+  else {
+    return 4;
+  }
+}
 
 void sense(HUSKYLENSResult result)
 {
-  if(result.ID == 1){
+  if(getValue(result) == 1){
     if(cC == 1){
+      Serial.print("You Tied. \T");
       //white light
     }
-    else if(cC == 2){
+    else if(cC == 3){
       //red light
+      Serial.print("You Lose Try Again. \T");
     }
     else{
       //green light
+      Serial.print("You Won YAY!. \T");
     }
 
   }
-  else if(result.ID == 2){
+  else if(getValue(result) == 3){
     if(cC == 1){
       //green light
+      Serial.print("You Won YAY!. \T");
     }
-    else if(cC == 2){
+    else if(cC == 3){
       //white light
+      Serial.print("You Tied. \T");
     }
     else{
       //red light
+      Serial.print("You Lose Try Again. \T");
     }
   }
-  else{
+  else if (getValue(result) == 2){
     if(cC == 1){
       //red light
+      Serial.print("You Lose Try Again. \T");
     }
-    else if(cC == 2){
+    else if(cC == 3){
       //green light
+      Serial.print("You Won YAY!. \T");
     }
     else{
       //white light
+      Serial.print("You Tied. \T");
     }
   }
   
+}
+
+void setup() {
+  Serial.begin(115200);
+  mySerial.begin(9600);
+  Serial.println(F("Guten Morgen1"));
+  Serial.println(F("Guten Morgen2"));
+  while (!huskylens.begin(mySerial)) {
+    Serial.println(F("no"));
+  }
+  Serial.println(F("yes1"));
+  Serial.println(F("yes2"));
+  pinMode(motor1pin1, OUTPUT);
+  pinMode(motor1pin2, OUTPUT);
+  pinMode(motor2pin1, OUTPUT);
+  pinMode(motor2pin2, OUTPUT);
+  // pinMode(huskyLens1, INPUT);
+  // pinMode(huskyLens2, INPUT);
+  // pinMode(EN_A, OUTPUT);
+  // pinMode(EN_B, OUTPUT);
+
+}
+
+void loop() {
+
+  if (!huskylens.request()) {
+      Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection!"));
+    }
+    else if(!huskylens.isLearned()){ 
+      Serial.println(F("Nothing learned, press learn button on HUSKYLENS to learn one!"));
+    }
+    else if(!huskylens.available()) {
+      Serial.println(F("No block or arrow appears on the screen!"));
+    }
+    
+    else
+    {
+        Serial.println(F("###########"));
+        while (huskylens.available())
+        {
+            HUSKYLENSResult result = huskylens.read();
+            printResult(result);
+            // sense(result);
+            if(getValue(result) == 1){
+              Serial.println(F("ROCK"));
+            }
+            else if(getValue(result) == 2){
+              Serial.println(F("SCISSORS"));
+            }
+            else if(getValue(result) == 3){
+              Serial.println(F("PAPER"));
+            }
+        }    
+    }
+  long random = randomChoice();
+  if (random == 1) {
+    rock();
+  }
+  else if (random == 3) {
+    paper();
+  }
+  else {
+    scissors();
+  }
+  delay(3000);
+  stopAllMotors();
+  delay(3000);
+  rock();
+  delay(3000);
+  stopAllMotors();
+  delay(3000);
+}
+
+void stopAllMotors(){
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, LOW);
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, LOW);
+}
+
+void motor1Forward() {
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+  // digitalWrite(EN_A, HIGH);
+}
+
+void motor1Backward() {
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, HIGH);
+  // digitalWrite(EN_A, HIGH);
+}
+
+void motor2Forward() {
+  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin2, LOW);
+  // digitalWrite(EN_B, HIGH);
+}
+
+void motor2Backward() {
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, HIGH);
+  // digitalWrite(EN_B, HIGH);
+}
+
+// Needs hand in Rock position before
+void scissors() {
+  if (cC == 1) {
+    motor1Forward();
+  }
+  else if (cC == 3) {
+    motor2Backward();
+  }
+  delay(3000);
+  cC = 2;
+}
+// needs hand in paper position before
+void rock() {
+  if (cC == 3){
+    motor1Backward();
+    motor2Backward();
+  }
+  else if (cC == 2) {
+    motor1Backward();
+  }
+  delay(3000);
+  cC = 1;
+}
+// needs hand in rock position before
+void paper() {
+  if (cC == 1){
+    motor1Forward();
+    motor2Forward();
+  }
+  else if (cC == 2) {
+    motor2Forward();
+  }
+  delay(3000);
+  cC = 3;
+}
+
+long randomChoice() {
+  return random(1, 4) ;
 }
 
